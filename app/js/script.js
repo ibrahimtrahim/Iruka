ba382a189478cb55868fb3b0d3e0ecc475ef68c2
ibrahimtrahim <trahimbrahim@gmail.com>
2023-07-11 00:55:26 +0000

    iRuka project

---
+++ app/js/script.js
@@ -0,0 +1,24 @@
+const carousel = document.querySelector(".clients__flex");
+
+let isDragging = false, startX, startScrollLeft;
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragging) return;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("moveup", dragStop);
